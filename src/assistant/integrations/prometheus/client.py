fcbf176661c5779fa8cb5bc4c06c33a8939ab5ae
Asher Foa <asher@userclouds.com>
2024-12-25 19:01:26 -0500

    add get_metric_metadata + force named args on prometheus client

--- src/assistant/integrations/prometheus/client.py
+++ src/assistant/integrations/prometheus/client.py
@@ -6,7 +6,7 @@ def get_prometheus_client(port: int = 9095):
 
 
 class PrometheusClient:
-    def __init__(self, base_url: str) -> None:
+    def __init__(self, *, base_url: str) -> None:
         self._client = httpx.Client(base_url=base_url)
 
     def get_alerts(self) -> list[dict]:
@@ -15,7 +15,7 @@ class PrometheusClient:
         response.raise_for_status()
         return response.json()["data"]["alerts"]
 
-    def get_alert_query(self, alert: dict) -> list[dict]:
+    def get_alert_query(self, *, alert: dict) -> list[dict]:
         # https://prometheus.io/docs/prometheus/latest/querying/api/#rules
         alertname = alert["labels"]["alertname"]
 
@@ -30,21 +30,29 @@ class PrometheusClient:
         alert_rule = groups[0]["rules"][0]
         return alert_rule["query"]
 
-    def query(self, promql_query: str) -> list[dict]:
-        response = self._client.get("/api/v1/query", params={"query": promql_query})
+    def query(self, *, query: str) -> list[dict]:
+        response = self._client.get("/api/v1/query", params={"query": query})
         response.raise_for_status()
         return response.json()
 
-    def get_metric_labels(self, metric_name: str) -> list[str]:
+    def get_metric_labels(self, *, metric_name: str) -> list[str]:
         # https://prometheus.io/docs/prometheus/latest/querying/api/#getting-label-names
         response = self._client.get("/api/v1/labels", params={"match[]": metric_name})
         response.raise_for_status()
         return response.json()["data"]
 
-    def get_metric_label_values(self, metric_name: str, label_name: str) -> list[str]:
+    def get_metric_label_values(
+        self, *, metric_name: str, label_name: str
+    ) -> list[str]:
         # https://prometheus.io/docs/prometheus/latest/querying/api/#querying-label-values
         response = self._client.get(
             f"/api/v1/label/{label_name}/values", params={"match[]": metric_name}
         )
+        response.raise_for_status()
+        return response.json()["data"]
+
+    def get_metric_metadata(self, *, metric_name: str) -> dict:
+        # https://prometheus.io/docs/prometheus/latest/querying/api/#querying-metric-metadata
+        response = self._client.get(f"/api/v1/metadata", params={"metric": metric_name})
         response.raise_for_status()
         return response.json()["data"]
