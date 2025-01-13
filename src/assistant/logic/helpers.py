77f482c9b9a16b771995486e6f0fbd68d1dfe0ec
Asher Foa <asher@userclouds.com>
2025-01-12 21:35:21 -0500

    Move logic to StreamHandler

--- src/assistant/logic/helpers.py
+++ src/assistant/logic/helpers.py
@@ -16,62 +16,11 @@ def extract_json_tag_content(text: str, tag_name: str) -> dict | list | None:
     return json.loads(content) if content else None
 
 
-class StreamMode(Enum):
-    NORMAL = "normal"
-    COLLECTING_TAG = "collecting_tag"
-    IN_TAG = "in_tag"
-
-
-class StreamTagExtractor:
-    def __init__(self, on_message_callback: Callable[..., None], on_tag_callback):
-        self._mode = StreamMode.NORMAL
-        self._tag_name_buffer = []
-        self._current_tag_name = None
-        self._tag_chunk_buffer = []
-        self._message_buffer = []
+class StreamHandler:
+    def __init__(self, on_message_callback: Callable[..., None]):
         self._on_message_callback = on_message_callback
-        self._on_tag_callback = on_tag_callback
         self._message_queue = None
-        self._active_tasks = set()
-
-    def reset_tags_tracker(self):
-        self._tag_name_buffer.clear()
-        self._current_tag_name = None
-        self._tag_chunk_buffer.clear()
-        self._tag_chunk_buffer.append("<")
-
-    async def _maybe_send_message(self, is_final: bool):
-        if not self._message_buffer:
-            if is_final and self._message_queue:
-                await self._message_queue.put(None)
-                self._message_queue = None
-            return
-        if not self._message_queue:
-            self._message_queue = await self._create_message_stream()
-        mb = "".join(self._message_buffer)
-        self._message_buffer.clear()
-        await self._message_queue.put(mb)
-        if is_final:
-            await self._message_queue.put(None)
-            self._message_queue = None
-
-    async def _start_tag(self):
-        self._mode = StreamMode.COLLECTING_TAG
-        self.reset_tags_tracker()
-        await self._maybe_send_message(is_final=False)
-
-    def _end_tag(self):
-        self._current_tag_name = "".join(self._tag_chunk_buffer)[1:-1]
-        self._mode = StreamMode.IN_TAG
-
-    def _in_tag_content(self, char):
-        self._tag_chunk_buffer.append(char)
-        assert self._current_tag_name is not None
-        tag_chunk = "".join(self._tag_chunk_buffer)
-        if not tag_chunk.endswith(f"</{self._current_tag_name}>"):
-            return
-        self._mode = StreamMode.NORMAL
-        self._on_tag_callback(self._current_tag_name, tag_chunk)
+        self._active_tasks: set[asyncio.Task] = set()
 
     def _get_stream_handler(self, queue):
         async def stream_handler() -> AsyncGenerator[str, None]:
@@ -94,6 +43,70 @@ class StreamTagExtractor:
         self._add_task(self._on_message_callback(sh()))
         return queue
 
+    async def maybe_send_message(self, message_buffer: list[str], is_final: bool):
+        if not message_buffer:
+            if is_final and self._message_queue:
+                await self._message_queue.put(None)
+                self._message_queue = None
+            return
+        if not self._message_queue:
+            self._message_queue = await self._create_message_stream()
+        mb = "".join(message_buffer)
+        await self._message_queue.put(mb)
+        if is_final:
+            await self._message_queue.put(None)
+            self._message_queue = None
+
+    async def wait_for_tasks(self):
+        if not self._active_tasks:
+            return
+        await asyncio.gather(*self._active_tasks)
+
+
+class StreamMode(Enum):
+    NORMAL = "normal"
+    COLLECTING_TAG = "collecting_tag"
+    IN_TAG = "in_tag"
+
+
+class StreamTagExtractor:
+    def __init__(self, on_message_callback: Callable[..., None], on_tag_callback):
+        self._mode = StreamMode.NORMAL
+        self._tag_name_buffer = []
+        self._current_tag_name = None
+        self._tag_chunk_buffer = []
+        self._message_buffer = []
+        self._on_tag_callback = on_tag_callback
+        self._stream_helper = StreamHandler(on_message_callback)
+
+    def reset_tags_tracker(self):
+        self._tag_name_buffer.clear()
+        self._current_tag_name = None
+        self._tag_chunk_buffer.clear()
+        self._tag_chunk_buffer.append("<")
+
+    async def _maybe_send_message(self, is_final: bool):
+        await self._stream_helper.maybe_send_message(self._message_buffer, is_final)
+        self._message_buffer.clear()
+
+    async def _start_tag(self):
+        self._mode = StreamMode.COLLECTING_TAG
+        self.reset_tags_tracker()
+        await self._maybe_send_message(is_final=False)
+
+    def _end_tag(self):
+        self._current_tag_name = "".join(self._tag_chunk_buffer)[1:-1]
+        self._mode = StreamMode.IN_TAG
+
+    def _in_tag_content(self, char):
+        self._tag_chunk_buffer.append(char)
+        assert self._current_tag_name is not None
+        tag_chunk = "".join(self._tag_chunk_buffer)
+        if not tag_chunk.endswith(f"</{self._current_tag_name}>"):
+            return
+        self._mode = StreamMode.NORMAL
+        self._on_tag_callback(self._current_tag_name, tag_chunk)
+
     async def handle_token(self, token: str) -> None:
         for char in token:
             if self._mode == StreamMode.NORMAL:
@@ -111,6 +124,4 @@ class StreamTagExtractor:
         await self._maybe_send_message(is_final=True)
 
     async def wait_for_tasks(self):
-        if not self._active_tasks:
-            return
-        await asyncio.gather(*self._active_tasks)
+        await self._stream_helper.wait_for_tasks()
